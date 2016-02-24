d31f8140944930250fbf982a0c11a4618cf7f5ab
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-02-24 06:54:16 +0200

    pythonised code

--- Python/numeralconverter.py
+++ Python/numeralconverter.py
@@ -20,19 +20,18 @@ def main(args):
         else:
             print("\"" + tmpString + "\" isn't 'a' or 'r'!")
     else:
-        select case args(0)
-            case "-h":
-                print("NumeralConverter - github.com/Walkman100/NumeralConverter")
-                printUsage()
-            case "-a"
-                if len(args) > 1: checkAndOutputRomanNumeral(args(1))
-                else: checkAndOutputRomanNumeral(raw_input("Enter Arabic number: "))
-            case "-r"
-                if len(args) > 1: outputArabicNumber(args(1)
-                else: outputArabicNumber(raw_input("Enter Roman number: "))
-            case else:
-                print("Unrecognised flag \"" + args(0) + "\"!")
-                printUsage()
+        if args(0) == "-h":
+            print("NumeralConverter - github.com/Walkman100/NumeralConverter")
+            printUsage()
+        elif args(0) == "-a":
+            if len(args) > 1: checkAndOutputRomanNumeral(args(1))
+            else: checkAndOutputRomanNumeral(raw_input("Enter Arabic number: "))
+        elif args(0) == "-r":
+            if len(args) > 1: outputArabicNumber(args(1))
+            else: outputArabicNumber(raw_input("Enter Roman number: "))
+        else:
+            print("Unrecognised flag \"" + args(0) + "\"!")
+            printUsage()
 
 def printUsage():
     flags = " [-h|-r [roman number]|-a [arabic number]]"
@@ -43,15 +42,15 @@ def checkAndOutputRomanNumeral(input):
         if len(input) < 19:
             outputRomanNumeral(input)
         else:
-            print("\"" + input + "\" is " + (len(input) - 18 + " digit(s) too long! Maximum size for \"\\\" operations is 18 digits")
+            print("\"" + input + "\" is " + (len(input) - 18 + " digit(s) too long! Maximum size for \"//\" operations is 18 digits"))
     else:
         print("\"" + input + "\" is not an Arabic number!")
 
 def outputRomanNumeral(number):
     if number > 1000:
-        for i=1 to (number \ 1000):
+        for i in range(1, (number // 1000) +1):
             print("M")
-        number = number - (number \ 1000) * 1000
+        number = number - (number // 1000) * 1000
     while number > 900:
         number = number - 900
         print("CM")
@@ -98,63 +97,58 @@ def outputRomanNumeral(number):
 
 def outputArabicNumber(RomanNumber):
     RomanNumber = RomanNumber.upper
-    for i=0 to len(RomanNumber):
-        select case RomanNumber(i):
-            case "I":
-                RomanNumber(i) = 1
-            case "V":
-                RomanNumber(i) = 2
-            case "X":
-                RomanNumber(i) = 3
-            case "L":
-                RomanNumber(i) = 4
-            case "C":
-                RomanNumber(i) = 5
-            case "D":
-                RomanNumber(i) = 6
-            case "M":
-                RomanNumber(i) = 7
-            case else:
-                print("\"" + RomanNumber(i) + "\" is not a valid Roman Numeral character!")
-                exit()
-    next
+    for i in range(0, len(RomanNumber +1)):
+        if RomanNumber(i) == "I":
+            RomanNumber(i) = 1
+        elif RomanNumber(i) == "V":
+            RomanNumber(i) = 2
+        elif RomanNumber(i) == "X":
+            RomanNumber(i) = 3
+        elif RomanNumber(i) == "L":
+            RomanNumber(i) = 4
+        elif RomanNumber(i) == "C":
+            RomanNumber(i) = 5
+        elif RomanNumber(i) == "D":
+            RomanNumber(i) = 6
+        elif RomanNumber(i) == "M":
+            RomanNumber(i) = 7
+        else:
+            print("\"" + RomanNumber(i) + "\" is not a valid Roman Numeral character!")
+            exit()
     # Now we have the roman number in arabic numbers (so we can use < and >), we just add it all
     ArabicNumber = 0
     RomanNumber = RomanNumber + "0" # Because loops, length calculation and next letter calculation
-    for i=0 to len(RomanNumber) - 1:
-        if i < len(RomanNumber) - 1 andalso RomanNumber(i) >= RomanNumber(i + 1):
-            select case RomanNumber(i):
-                case "1":
-                    ArabicNumber = ArabicNumber + 1
-                case "2":
-                    ArabicNumber = ArabicNumber + 5
-                case "3":
-                    ArabicNumber = ArabicNumber + 10
-                case "4":
-                    ArabicNumber = ArabicNumber + 50
-                case "5":
-                    ArabicNumber = ArabicNumber + 100
-                case "6":
-                    ArabicNumber = ArabicNumber + 500
-                case "7":
-                    ArabicNumber = ArabicNumber + 1000
+    for i in range(0, len(RomanNumber)):
+        if i < len(RomanNumber) - 1 and RomanNumber(i) >= RomanNumber(i + 1):
+            if RomanNumber(i) == "1":
+                ArabicNumber = ArabicNumber + 1
+            elif RomanNumber(i) == "2":
+                ArabicNumber = ArabicNumber + 5
+            elif RomanNumber(i) == "3":
+                ArabicNumber = ArabicNumber + 10
+            elif RomanNumber(i) == "4":
+                ArabicNumber = ArabicNumber + 50
+            elif RomanNumber(i) == "5":
+                ArabicNumber = ArabicNumber + 100
+            elif RomanNumber(i) == "6":
+                ArabicNumber = ArabicNumber + 500
+            elif RomanNumber(i) == "7":
+                ArabicNumber = ArabicNumber + 1000
         elif i < len(RomanNumber) - 1:
-            select case RomanNumber(i)
-                case "1":
-                    ArabicNumber = ArabicNumber - 1
-                case "2":
-                    ArabicNumber = ArabicNumber - 5
-                case "3":
-                    ArabicNumber = ArabicNumber - 10
-                case "4":
-                    ArabicNumber = ArabicNumber - 50
-                case "5":
-                    ArabicNumber = ArabicNumber - 100
-                case "6":
-                    ArabicNumber = ArabicNumber - 500
-                case "7": # logic says this can't be possible, but no real reason to leave it out...
-                    ArabicNumber = ArabicNumber - 1000
-    next
+            if RomanNumber(i) == "1":
+                ArabicNumber = ArabicNumber - 1
+            elif RomanNumber(i) == "2":
+                ArabicNumber = ArabicNumber - 5
+            elif RomanNumber(i) == "3":
+                ArabicNumber = ArabicNumber - 10
+            elif RomanNumber(i) == "4":
+                ArabicNumber = ArabicNumber - 50
+            elif RomanNumber(i) == "5":
+                ArabicNumber = ArabicNumber - 100
+            elif RomanNumber(i) == "6":
+                ArabicNumber = ArabicNumber - 500
+            elif RomanNumber(i) == "7": # logic says this can't be possible, but no real reason to leave it out...
+                ArabicNumber = ArabicNumber - 1000
     print(ArabicNumber)
 
 main(sys.argv[1:])
