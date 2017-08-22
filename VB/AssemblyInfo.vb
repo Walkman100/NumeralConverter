28a812a276e57469a31b7ed5dea57cb7ec2a527c
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-08-22 17:35:16 +0200

    Updated AssemblyInfo

--- VB/AssemblyInfo.vb
+++ VB/AssemblyInfo.vb
@@ -4,11 +4,11 @@ Imports System.Runtime.InteropServices
 
 <assembly: AssemblyTitle("NumeralConverter")>
 <assembly: AssemblyDescription("Convert between Arabic Numerals and Roman Numerals")>
-<assembly: AssemblyCompany("Walkman100")>
+<assembly: AssemblyCompany("WalkmanOSS")>
 <assembly: AssemblyProduct("NumeralConverter")>
-<assembly: AssemblyCopyright("FOSS from Walkman 2015")>
+<assembly: AssemblyCopyright("FOSS from Walkman 2015-17")>
 
 <assembly: ComVisible(False)>
 
-<Assembly: AssemblyVersion("1.2.0.0")>
-<Assembly: AssemblyFileVersion("1.2.0.0")>
+<Assembly: AssemblyVersion("1.2.1.0")>
+<Assembly: AssemblyFileVersion("1.2.1.0")>
